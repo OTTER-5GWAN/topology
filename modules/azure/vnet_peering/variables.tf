a0dc9f44182507ed636dee1d82aafbd4d9f80f32
OTTER-5GWAN <125412068+OTTER-5GWAN@users.noreply.github.com>
2023-08-21 13:38:37 -0700

    new release

---
+++ modules/azure/vnet_peering/variables.tf
@@ -0,0 +1,30 @@
+variable "rg" {
+  description = "Resource group."
+  type        = string
+}
+
+variable "vnet1_name" {
+  description = "Name of first VNet."
+  type        = string
+}
+
+variable "vnet1_id" {
+  description = "ID of first VNet."
+  type        = string
+}
+
+variable "vnet2_name" {
+  description = "Name of second VNet."
+  type        = string
+}
+
+variable "vnet2_id" {
+  description = "ID of second VNet."
+  type        = string
+}
+
+variable "use_gateways" {
+  description = "Whether to use gateways."
+  type        = bool
+  default     = false
+}
