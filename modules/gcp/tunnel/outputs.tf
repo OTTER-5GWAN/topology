2526728eda9b7674e687831aea4d51dbfe5aac90
OTTER-5GWAN <125412068+OTTER-5GWAN@users.noreply.github.com>
2023-08-21 13:26:13 -0700

    cleanup for new version of code

--- modules/gcp/tunnel/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "tunnel_key" {
-    description = "IKE2 tunnel key."
-    value = google_compute_vpn_tunnel.tunnel.shared_secret
-}
-
-output "bgp_peer_ip_addr" {
-    description = "BGP peering address of the peer (Azure) interface."
-    value = google_compute_router_peer.peer.peer_ip_address
-}
